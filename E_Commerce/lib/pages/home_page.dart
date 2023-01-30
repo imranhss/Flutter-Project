c6ac5c5af3ac9c37cb80f4640fb565bcb24466d1
imranhss <emranhss@gmail.com>
2023-01-31 00:02:26 +0600

    Theme Extracted

--- E_Commerce/lib/pages/home_page.dart
+++ E_Commerce/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/widget/drawer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class HomePage extends StatelessWidget {
             child: Text("Hello")
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
