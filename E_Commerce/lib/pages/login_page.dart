2de86bba894f6a3e8cd5c4a0c6683ab2a49b2af9
imranhss <emranhss@gmail.com>
2023-02-19 20:33:04 +0600

    Update login_page.dart

--- E_Commerce/lib/pages/login_page.dart
+++ E_Commerce/lib/pages/login_page.dart
@@ -26,6 +26,7 @@ class LoginPage extends StatelessWidget {
                 fontWeight: FontWeight.w900,
                 color: Colors.orangeAccent,
               ),
+<<<<<<< Updated upstream
             ),
             SizedBox(
               height: 40.0,
@@ -53,6 +54,94 @@ class LoginPage extends StatelessWidget {
                       "Login",
                       style: TextStyle(
                         fontSize: 20,
+=======
+              Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Welcome $name to Hotel Rents",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.orangeAccent,
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 32.0),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "Enter User Name"),
+                      //for Validation as like Bootstrape
+                    validator: (value) {
+                        if (value!.isEmpty) {
+                          return "User Name can not be empty";
+                        }
+                        return null;
+                      },
+                      // on change effect
+                      onChanged: (value) {
+                        this.name = value;
+                        // Reload the UI
+                        setState(() {});
+                      },
+
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(hintText: "Enter Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password can not be empty";
+                        }
+                        else if (value.length<6) {
+                          return "Password length should be minimum 6";
+                        }
+
+                      },
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    InkWell(
+                      onTap: () => moveToHome(context),
+
+
+                      
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        height: 40,
+                        width: changeButton ? 50 : 150,
+                        // placed to decoration
+                        // color: Colors.deepPurple,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(
+                          Icons.done,
+                          color: Colors.white,
+                        )
+                            : Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 10),
+                        ),
+>>>>>>> Stashed changes
                       ),
                     ),
                     style: TextButton.styleFrom(
