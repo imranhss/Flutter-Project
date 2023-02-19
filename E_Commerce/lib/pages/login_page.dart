7a43022635343978c4a61575d741b1719cf9b755
imranhss <emranhss@gmail.com>
2023-02-19 20:35:04 +0600

    Fix Error

--- E_Commerce/lib/pages/login_page.dart
+++ E_Commerce/lib/pages/login_page.dart
@@ -1,60 +1,44 @@
 import 'package:e_commerce/util/route.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(
+        seconds: 1,
+      ));
+      await Navigator.pushNamed(context, MyRoute.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 40.0,
-            ),
-            Image.asset(
-              "assets/images/logo.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome to Hotel Rents",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w900,
-                color: Colors.orangeAccent,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40.0,
               ),
-<<<<<<< Updated upstream
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(hintText: "Enter User Name"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(hintText: "Enter Password"),
-                  ),
-                  SizedBox(
-                    height: 40.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoute.homeRoute);
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 20,
-=======
               Image.asset(
                 "assets/images/logo.png",
                 fit: BoxFit.cover,
@@ -81,7 +65,7 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(hintText: "Enter User Name"),
                       //for Validation as like Bootstrape
-                    validator: (value) {
+                      validator: (value) {
                         if (value!.isEmpty) {
                           return "User Name can not be empty";
                         }
@@ -113,9 +97,6 @@ class LoginPage extends StatelessWidget {
                     ),
                     InkWell(
                       onTap: () => moveToHome(context),
-
-
-                      
                       child: AnimatedContainer(
                         duration: Duration(seconds: 1),
                         height: 40,
@@ -141,17 +122,13 @@ class LoginPage extends StatelessWidget {
                           borderRadius:
                           BorderRadius.circular(changeButton ? 50 : 10),
                         ),
->>>>>>> Stashed changes
                       ),
                     ),
-                    style: TextButton.styleFrom(
-                      minimumSize: Size(150, 40),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
