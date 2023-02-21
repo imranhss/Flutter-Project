1becd430b4e16577a63fbbd347d779996c95a1dd
imranhss <emranhss@gmail.com>
2023-02-22 00:31:38 +0600

    asd

--- E_Commerce/lib/pages/home_page.dart
+++ E_Commerce/lib/pages/home_page.dart
@@ -6,36 +6,35 @@ import 'package:flutter/services.dart';
 import '../models/catalog.dart';
 import 'dart:convert';
 
-
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     loadData();
-
   }
 
-  loadData()async{
-   var catalogJson= await  rootBundle.loadString("assets/files/catalog.json");
-   print(catalogJson);
-   var decodeData=jsonDecode(catalogJson);
-   print(decodeData);
-   var productsData=decodeData["products"];
-   print(productsData);
-   CatalogModel.item= List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
 
-   setState(() {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    //print(catalogJson);
+    var decodeData = jsonDecode(catalogJson);
+    //print(decodeData);
+    var productsData = decodeData["products"];
+    // print(productsData);
+    CatalogModel.item = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
 
-   });
+    setState(() {});
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -47,23 +46,60 @@ class _HomePageState extends State<HomePage> {
         title: Text("Hotel Rents"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          // for check
-          itemCount: CatalogModel.item.length,
-          // itemCount:dummyList.length,
-          itemBuilder: (context, index){
-            return ItemWidget(
-              // for check
-              // item: CatalogModel().items[index],
-              // item: dummyList[index],
-              item: CatalogModel.item[index],
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+              ),
+              itemCount: CatalogModel.item.length,
+              itemBuilder: (context, index) {
+                final it = CatalogModel.item[index];
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: GridTile(
+                    header: Container(
+                      padding: const EdgeInsets.all(12.0),
 
-            );
-          },
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                        child: Text(it.name,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        )
+                    ),
+                    child: Image.network(it.image,
+                    fit: BoxFit.cover,
+                      height: 50,
+                    ),
+                    footer: Text(it.price.toString()),
+                  ),
+                );
+              })
+          // ---------------End Grid View----------------------
 
-        ),
-      ),
+          // ListView.builder(
+          //   // for check
+          //   itemCount: CatalogModel.item.length,
+          //   // itemCount:dummyList.length,
+          //   itemBuilder: (context, index){
+          //     return ItemWidget(
+          //       // for check
+          //       // item: CatalogModel().items[index],
+          //       // item: dummyList[index],
+          //       item: CatalogModel.item[index],
+          //
+          //     );
+          //   },
+          //
+          // ),
+          ),
       drawer: MyDrawer(),
     );
   }
